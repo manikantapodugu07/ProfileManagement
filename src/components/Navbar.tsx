e963ae5c736a7e7bac4107991e11343afb1ede06
manikantapodugu07 <manikantapodugu27@gmail.com>
2025-11-07 22:23:15 +0530

    code changed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+//Here the nav bar is created
+
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
